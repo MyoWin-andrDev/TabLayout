b621a4ea467b9f4caf1ec19b7faa79e48341989d
Phyo Hein ( Ezekiel ) <mwrcps11@gmail.com>
2025-06-06 22:25:44 +0700

    Fix Slow Loading With Glide

--- app/src/main/java/com/learning/tablayout/adapter/CustomAdapter.kt
+++ app/src/main/java/com/learning/tablayout/adapter/CustomAdapter.kt
@@ -3,24 +3,29 @@ package com.learning.tablayout.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.learning.tablayout.databinding.ListItemTablayoutBinding
-import com.learning.tablayout.model.CustomList
+import com.learning.tablayout.model.EntityModel
 
-class CustomAdapter(private val customList : List<CustomList>) : RecyclerView.Adapter<CustomAdapter.CustomViewHolder>() {
+class CustomAdapter(private val customList : List<EntityModel>) : RecyclerView.Adapter<CustomAdapter.CustomViewHolder>() {
 
     class CustomViewHolder(val binding : ListItemTablayoutBinding) : RecyclerView.ViewHolder(binding.root){
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder =  CustomViewHolder(ListItemTablayoutBinding.inflate(LayoutInflater.from(parent.context)))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder =  CustomViewHolder(ListItemTablayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false))
 
     override fun getItemCount(): Int = customList.size
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         holder.binding.apply {
-            tvName.text = customList[position].name
-            tvDescription.text = customList[position].description
-            ivImage.setImageResource(customList[position].image)
+            val item = customList[position]
+            tvName.text = item.name
+            tvDescription.text = item.description
+            Glide.with(root.context)
+                .load(item.image)
+                .thumbnail(0.1f)
+                .into(ivImage)
         }
     }
 }
---
+++ app/src/main/java/com/learning/tablayout/model/EntityModel.kt
@@ -0,0 +1,3 @@
+package com.learning.tablayout.model
+
+data class EntityModel(val name : String, val description : String, val image : Int)
